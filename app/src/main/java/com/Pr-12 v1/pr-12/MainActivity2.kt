853b16aae5d094cba7bf39f2b72187cb4b5811da
Andrey <andrey.bechikov2003@mail.ru>
2022-06-18 13:10:05 +0700

    first commit

---
+++ app/src/main/java/com/Pr-12 v1/pr-12/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.`Pr-12 v1`.`pr-12`
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pr12antuh.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
---
+++ app/src/test/java/com/Pr-12 v1/pr-12/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.`Pr-12 v1`.`pr-12`
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
